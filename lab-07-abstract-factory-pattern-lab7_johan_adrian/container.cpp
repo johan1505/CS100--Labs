1cda9954da519fa08e19f1806f7d9806c6d1c8ac
Johan Guzman <jguzm022@ucr.edu>
2019-06-15 21:35:08 -0700

    Cleaning

--- lab-07-abstract-factory-pattern-lab7_johan_adrian/container.cpp
+++
@@ -1,8 +0,0 @@
-#include "container.hpp"
-#include "sort.hpp"
-#include "base.hpp"
-
-void Container::set_sort_function(Sort* sort_function){
-                this->sort_function = sort_function;
-                return;
-} 
